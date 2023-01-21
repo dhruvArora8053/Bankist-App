49979a65bb6f3dc6151a2a0059045fc0277f9ddb
Dhruv Arora <dhruvarora.code@gmail.com>
2023-01-21 17:53:05 +0530

    hello

--- script.js
+++ script.js
@@ -61,3 +61,4 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+console.log("hello world");
